838f1e7cf81c518bd000527e8aa688d7b4f2c310
Idriss Matos <imatos@mozdomains.com>
2022-09-22 21:25:56 +0200

    config.dart

--- libs/client-shared/lib/config.dart
+++ libs/client-shared/lib/config.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 import './map_providers.dart';
 import 'package:latlong2/latlong.dart';
 
-String serverIP = "x.x.x.x";
+String serverIP = "207.154.244.112";
 
 bool demoMode = false;
 String companyName = "ridy.io";
@@ -16,8 +16,8 @@ String mapBoxTileSetId = "streets-v11";
 
 String loginTermsAndConditionsUrl = "";
 
-String defaultCurrency = "USD";
-String defaultCountryCode = "+1";
+String defaultCurrency = "MZN";
+String defaultCountryCode = "+258";
 const List<double> tipPresets = [10, 20, 50];
 
 LatLng fallbackLocation = LatLng(37.3382, -121.8863);
